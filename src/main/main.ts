61b3a0be3da64a23757ad6ebf70f8c24885deca9
dragonDScript <agomila96@gmail.com>
2021-02-10 22:26:06 +0100

    Improve security

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,7 +3,11 @@ import { join } from 'path'
 
 app.whenReady().then(() => {
   const win = new BrowserWindow({
-
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      enableRemoteModule: false
+    }
   })
   win.loadURL(app.isPackaged ? 'file://' + join(process.cwd(), 'index.html') : 'http://localhost:3000')
   win.setMenuBarVisibility(false)
